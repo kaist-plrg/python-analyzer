78cad4e61174b600836e8e9c3217d54627eddb83
Wonho Shin <wonhoshin@iMac-4.local>
2021-05-21 15:02:50 +0900

    Fixed bug in string literal

--- src/main/scala/kr/ac/kaist/pyanalyzer/parser/Tokenizer.scala
+++ src/main/scala/kr/ac/kaist/pyanalyzer/parser/Tokenizer.scala
@@ -78,16 +78,18 @@ trait Tokenizers extends RegexParsers {
   ).mkString("|").r ^^ { case s => Keyword(s)}
 
   // literals
-  lazy val singleQuote = "['\"].*['\"]".r
-  lazy val doubleQuote = "[(''')(\"\"\")].*[(''')(\"\"\")]".r
+  lazy val quote = "['\"]".r
+  lazy val tripleQuote = "['\"]{3}".r
+  lazy val shortQuote = quote ~> "[^'\"]*".r <~ quote
+  lazy val longQuote = tripleQuote ~> "[^'\"]*".r <~ tripleQuote
   lazy val stringPrefix = "r".r | "u".r | "R".r | "U".r | "f".r | "F".r |
     "fr".r | "Fr".r | "fR".r | "rf".r | "rF".r | "Rf".r | "RF".r
   lazy val stringLiteral: Parser[StrLiteral] = opt(stringPrefix) ~>
-    (singleQuote | doubleQuote) ^^ StrLiteral
+    (longQuote | shortQuote) ^^ StrLiteral
   lazy val bytesPrefix = "b".r | "B".r | "br".r | "bR".r | "BR".r |
     "rb".r | "rB".r | "Rb".r | "RB".r
   lazy val bytesLiteral: Parser[BytesLiteral] = bytesPrefix ~>
-    (singleQuote | doubleQuote) ^^ BytesLiteral
+    (longQuote | shortQuote) ^^ BytesLiteral
 
   // TODO escape character how?
 
