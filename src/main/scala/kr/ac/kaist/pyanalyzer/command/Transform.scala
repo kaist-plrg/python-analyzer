3046488a09319d90adf23b2ae6dce9bc7df12290
Wonho Shin <wonhoshin@iMac-4.local>
2021-07-30 13:35:25 +0900

    Add more options in Transform command

--- src/main/scala/kr/ac/kaist/pyanalyzer/command/Transform.scala
+++ src/main/scala/kr/ac/kaist/pyanalyzer/command/Transform.scala
@@ -10,63 +10,100 @@ import kr.ac.kaist.pyanalyzer.util.Useful._
 import scala.Console._
 
 object Transform {
+  val logPath = BASE_DIR + "/logs/transform"
+  mkdir(logPath)
+
   def apply(optionMap: Map[String, String]): Unit = {
+    // set target file
     val target = optionMap.get("target")
+    // set diff option
+    val diffOption = if (optionMap contains "y") "y" else "u"
+    // set diff stage
+    val stage = optionMap.getOrElse("stage", "hvd-trans")
+    // print all stage
+    val all = if (optionMap contains "all") true else false
+
     val files = walkTree(HOROVOD_DIR)
     for {
       file <- files
       orgPath = file.getPath()
-      if (orgPath endsWith ".py") && (orgPath contains "/org/")
+      if (orgPath endsWith ".py") && (orgPath contains "/org/") &&
+        readSource(orgPath).nonEmpty
       relPath = orgPath.drop(HOROVOD_DIR.length + 1)
       if target.map(relPath contains _).getOrElse(true)
+      hvdPath = s"$HOROVOD_DIR/${relPath.replace("/org/", "/hvd/")}"
     } {
-      // parse
+
       println
       println(s"$CYAN$relPath$RESET")
       try {
-        val orgText = readSource(orgPath)
-        if (orgText == "") println(s"${RED}empty file${RESET}")
-        else {
-          val orgTokens = tokenizeText(orgText)
-          val orgAst = TokenListParser(orgTokens)
-          val orgResult = beautify(orgAst)
-          // transform
-          val transformedAst = Transformer(orgAst)
-          val transformedResult = beautify(transformedAst)
-          val hvdPath = HOROVOD_DIR + "/" + 
-            relPath.replace("/org/", "/hvd/")
-          val hvdText = readSource(hvdPath)
-          val hvdTokens = tokenizeText(hvdText)
-          val hvdAst = TokenListParser(hvdTokens)
-          val hvdResult = beautify(hvdAst)
-          println
-          println(s"${CYAN}ORG${RESET}")
-          println(orgResult)
-          println("==================================================")
-          println(s"${CYAN}TRANS${RESET}")
-          println(transformedResult)
-          println("==================================================")
-          println(s"${CYAN}HVD${RESET}")
-          println(hvdResult)
-          println("==================================================")
-          val transLogPath = BASE_DIR + "/logs/transform"
-          mkdir(transLogPath)
-          dumpFile(transformedResult, transLogPath + "/trans")
-          dumpFile(hvdResult, transLogPath + "/hvd")
-          println(s"${CYAN}DIFF${RESET}")
-          try executeCmd(
-            s"colordiff -u ${transLogPath}/hvd ${transLogPath}/trans")
-          catch {
-            case e: Throwable =>
-              executeCmd(s"diff -u ${transLogPath}/hvd ${transLogPath}/trans")
-              println
-              println(s"${YELLOW}[Warning] install colordiff${RESET}")
-          }
-          executeCmd(s"rm ${transLogPath}/hvd ${transLogPath}/trans")
+        // org
+        val orgAst = parseFile(orgPath)
+        val orgResult = beautify(orgAst)
+        // transformed
+        val transformedAst = Transformer(orgAst)
+        val transformedResult = beautify(transformedAst)
+        // hvd
+        val hvdAst = parseFile(hvdPath)
+        val hvdResult = beautify(hvdAst)
+
+        // target stage
+        val comparePair = stage match {
+          case "org-hvd" =>
+            ("org", orgResult, "hvd", hvdResult)
+          case "org-trans" =>
+            ("org", orgResult, "trans", hvdResult)
+          case _ =>
+            ("hvd", hvdResult, "trans", transformedResult)
         }
+
+        // print result
+        if (all) {
+          printResult(orgResult, transformedResult, hvdResult)
+          println("==================================================")
+        }
+        printDiff(comparePair, diffOption)
       } catch {
         case e: Throwable => e.printStackTrace()
       }
     }
+  }
+
+  def printResult(
+    orgResult: String,
+    transformedResult: String,
+    hvdResult: String,
+  ): Unit = {
+    println
+    println(s"${CYAN}ORG${RESET}")
+    println(orgResult)
+    println("==================================================")
+    println(s"${CYAN}TRANS${RESET}")
+    println(transformedResult)
+    println("==================================================")
+    println(s"${CYAN}HVD${RESET}")
+    println(hvdResult)
+  }
+  def printDiff(
+    comparePair: (String, String, String, String),
+    diffOption: String
+  ): Unit = {
+    val (name1, content1, name2, content2) = comparePair
+    val path1 = s"$logPath/$name1"
+    val path2 = s"$logPath/$name2"
+    dumpFile(content1, path1)
+    dumpFile(content2, path2)
+    println(s"${CYAN}DIFF${RESET}")
+    try executeCmd(
+      s"colordiff -$diffOption $path1 $path2"
+    ) catch {
+      case e: Throwable =>
+        executeCmd(
+          s"diff -$diffOption $path1 $path2"
+        )
+        println
+        println(s"${YELLOW}[Warning] install colordiff${RESET}")
+    }
+    executeCmd(s"rm $path1 $path2")
   }
 }
