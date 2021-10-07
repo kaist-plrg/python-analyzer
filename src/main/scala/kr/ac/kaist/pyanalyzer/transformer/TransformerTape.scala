da7133b10235e2c96024822b5479a04fb7226b5d
Wonho Shin <wonhoshin@iMac-4.local>
2021-10-07 15:44:47 +0900

    Refactor TransformerTape

--- src/main/scala/kr/ac/kaist/pyanalyzer/transformer/TransformerTape.scala
+++ src/main/scala/kr/ac/kaist/pyanalyzer/transformer/TransformerTape.scala
@@ -13,37 +13,10 @@ import scala.Console._
 
 object TransformerTape extends Transformer {
   // transformed one AST into another AST
-  def apply(ast: Module): Module = ast match {
-    case m @ Module(body, tyIg, name) =>
-      Module(transform(body)(Env())._1, tyIg, name)
-  }
-
-  /////////////////////////////////////////
-  // transformer for statements
-  /////////////////////////////////////////
-  override def transform(stmts: List[Stmt])(implicit env: Env): (List[Stmt], Env) = 
-    stmts.foldLeft((List[Stmt](), env)) {
-      case ((stmtList, e), stmt) =>
-        val (newStmtList, newEnv) = transform(stmt)(e)
-        (stmtList ++ newStmtList, newEnv)
-    }
+  def apply(ast: Module): Module = 
+    ast.copy(body=transform(ast.body)(Env())._1)
 
   override def transform(stmt: Stmt)(implicit env: Env): (List[Stmt], Env) = stmt match {
-    // function override def
-    case FunDef(decos, name, args, retTy, tyExpr, body) =>
-      (FunDef(decos, name, args, retTy, tyExpr, transform(body)._1), env) 
-    case AsyncFunDef(decos, name, args, retTy, tyExpr, body) =>
-      (AsyncFunDef(decos, name, args, retTy, tyExpr, transform(body)._1), env) 
-
-    // class override def
-    case ClassDef(decos, name, exprs, kwds, body) =>
-      (ClassDef(decos, name, exprs, kwds, transform(body)._1), env)
-
-    // return, del
-    case ReturnStmt(eopt) =>
-      (ReturnStmt(eopt.map(expr => transform(expr))), env)
-    case DelStmt(tl) => (DelStmt(tl), env)
-
     /////////////////////////////////////////////////////////////////
     //// strict form of assignment
     /////////////////////////////////////////////////////////////////
@@ -129,15 +102,6 @@ object TransformerTape extends Transformer {
       EConst(StringLiteral("CUDA_VISIBLE_DEVICES")))), expr, ty) 
       if env.get("os") contains idt => 
         (List(), env)
-    /////////////////////////////////////////////////////////////////
-    // general form of assignment
-    /////////////////////////////////////////////////////////////////
-
-    // AssignStmt that targets is non-singular or non-id
-    case AssignStmt(targets, e, ty) => 
-      (AssignStmt(targets, transform(e), ty), env)
-    // AugAssign case
-    case AugAssign(lhs, bop, rhs) => (AugAssign(lhs, bop, transform(rhs)), env)
     // AnnAssign case: 
     case AnnAssign(e1, e2, e3) =>
       (e1, e3) match {
@@ -151,21 +115,6 @@ object TransformerTape extends Transformer {
         // case 2) otherwise
         case _ => (AnnAssign(e1, e2, e3.map(transform)), env)
       }
-
-
-    /////////////////////////////////////////////////////////////////
-    // for statement
-    case ForStmt(ty, forExpr, inExpr, doStmt, elseStmt) =>
-      (ForStmt(ty, forExpr, transform(inExpr), transform(doStmt)._1, transform(elseStmt)._1), env)
-    case AsyncForStmt(ty, forExpr, inExpr, doStmt, elseStmt) =>
-      (AsyncForStmt(ty, forExpr, transform(inExpr), transform(doStmt)._1, transform(elseStmt)._1), env)
-    // while statement
-    case WhileStmt(wExpr, doStmt, elseStmt) =>
-      (WhileStmt(transform(wExpr), transform(doStmt)._1, transform(elseStmt)._1), env) 
-    // if statement
-    case IfStmt(cond, thenStmt, elseStmt) =>
-      (IfStmt(transform(cond), transform(thenStmt)._1, transform(elseStmt)._1), env)
-
     /////////////////////////////////////////////////////////////////
     // with statement
     /////////////////////////////////////////////////////////////////
@@ -201,25 +150,6 @@ object TransformerTape extends Transformer {
           (newerStmts, newEnv)
         case _ => (AsyncWithStmt(ty, newItems, newStmts), newEnv)
       }
-
-    /////////////////////////////////////////////////////////////////
-    // match statement
-    /////////////////////////////////////////////////////////////////
-    case MatchStmt(expr, cases) =>
-      (MatchStmt(transform(expr), cases.map(c => transform(c))), env)  
-
-    // exception-related statements
-    case RaiseStmt(expr, from) =>
-      (RaiseStmt(expr, from), env)
-    case TryStmt(tryStmt, handlers, elseStmt, finallyStmt) =>
-      val newTryStmt =
-        TryStmt(
-          transform(tryStmt)._1, handlers.map(transform),
-          transform(elseStmt)._1, transform(finallyStmt)._1)
-      (newTryStmt, env)
-    case AssertStmt(expr, toRaise) =>
-      (AssertStmt(transform(expr), toRaise), env)
-
     /////////////////////////////////////////////////////////////////
     // importstmt
     /////////////////////////////////////////////////////////////////
@@ -236,12 +166,6 @@ object TransformerTape extends Transformer {
         // corresponding not found
         case _ => (ImportStmt(alias), newEnv)
       }
-
-    /////////////////////////////////////////////////////////////////
-    // other scope-related
-    case ImportFromStmt(lv, fromId, al) => (ImportFromStmt(lv, fromId, al), env)
-    case GlobalStmt(il) => (GlobalStmt(il), env)
-    case NonlocalStmt(il) => (NonlocalStmt(il), env)
 
     /////////////////////////////////////////////////////////////////
     // strict form of expr stmts
@@ -294,63 +218,13 @@ object TransformerTape extends Transformer {
       // case _) other expr stmts
       case _ => (ExprStmt(transform(Call(expr1, exprs, kwds))), env)
     }
-
-    /////////////////////////////////////////////////////////////////
-    // general form of expr
-    case ExprStmt(e) => (ExprStmt(transform(e)), env)
-    case PassStmt => (PassStmt, env)
-    case BreakStmt => (BreakStmt, env)
-    case ContinueStmt => (ContinueStmt, env)
-    // TODO: check transform from simple to compound exists
-    case OnelineStmt(ls) =>
-      val (newLs, newEnv) = transform(ls)
-      (OnelineStmt(newLs), newEnv)
-    case Comment(c) => (Comment(c), env)
+    case stmt => super.transform(stmt)
   }
 
   /////////////////////////////////////////
   // transformer for Expression
   /////////////////////////////////////////
   override def transform(expr: Expr)(implicit env: Env): Expr = expr match {
-    case BoolExpr(op, lhs, rhs) => 
-      BoolExpr(op, transform(lhs), transform(rhs))
-    case NamedExpr(lhs, rhs) => 
-      NamedExpr(lhs, transform(rhs))
-    case BinaryExpr(op, lhs, rhs) => 
-      BinaryExpr(op, transform(lhs), transform(rhs))
-    case UnaryExpr(op, e) => 
-      UnaryExpr(op, transform(e))
-    case LambdaExpr(args, e) => 
-      LambdaExpr(args, transform(e))
-    case IfExpr(e, cond, ee) => 
-      IfExpr(transform(e),transform(cond),transform(ee))
-    case DictExpr(map) => DictExpr(map.map(transform))
-    case SetExpr(set) => SetExpr(set.map(transform))
-    case ListExpr(list) => ListExpr(list.map(transform))
-    case TupleExpr(tup) => TupleExpr(tup.map(transform))
-    case DictComp((k, v), comps) => DictComp(
-      (k, transform(v)),
-      comps.map(transform)
-    )
-    case SetComp(target, comps) => SetComp(
-      transform(target),
-      comps.map(transform)
-    )
-    case ListComp(target, comps) => ListComp(
-      transform(target),
-      comps.map(transform)
-    )
-    case GenComp(target, comps) => GenComp(
-      transform(target),
-      comps.map(transform)
-    )
-    case AwaitExpr(e) => AwaitExpr(transform(e))
-  case YieldExpr(opt) => YieldExpr(opt.map(transform))
-    case YieldFromExpr(e) => YieldFromExpr(transform(e))
-    case CompExpr(head, lp) => CompExpr(
-      transform(head),
-      lp.map { case (op, e) => (op, transform(e)) }
-    )
     case Call(expr1, le, lk) => expr1 match {
       // case 1) σ("dataset") = id_t and expr_1 = id_t.take
       case Attribute(EName(idt), Id("take")) 
@@ -378,40 +252,8 @@ object TransformerTape extends Transformer {
         }
       )
     }
-    case FormattedValue(lhs, n, rhs) => FormattedValue(lhs, n, rhs)
-    case JoinedStr(le) => JoinedStr(le)
-    case EConst(e) => EConst(e)
-    case Attribute(e, x) => Attribute(e, x)
-    case Subscript(e, slice) => Subscript(transform(e), transform(slice))
-    case Starred(e) => Starred(e)
-    case EName(x) => EName(x)
-    case Slice(begin, end, step) => Slice(
-      begin.map(transform),
-      end.map(transform),
-      step.map(transform)
-    )
-    case GroupExpr(e) => GroupExpr(transform(e))
+    case expr => super.transform(expr)
   }
-
-  /////////////////////////////////////////
-  // transformers for sub-constructs
-  /////////////////////////////////////////
-  override def transform(comp: Comprehension)(implicit env: Env): Comprehension = 
-    comp match {
-      case Compre(target, in, conds) =>
-        Compre(target, transform(in), conds.map(transform))
-      case AsyncCompre(target, in, conds) =>
-        AsyncCompre(target, transform(in), conds.map(transform))
-    }
-
-  override def transform(handler: ExcHandler)(implicit env: Env): ExcHandler = 
-    handler match {
-      case ExcHandler(except, idOpt, body) =>
-        ExcHandler(except, idOpt, transform(body)._1)
-    }
-
-  override def transform(al: List[Alias])(implicit env: Env): Env = 
-    al.foldLeft(env)((e, a) => transform(a)(e))
 
   override def transform(alias: Alias)(implicit env: Env): Env = alias match {
     case Alias(List(x), None) if x.name == "tensorflow" =>
@@ -429,14 +271,6 @@ object TransformerTape extends Transformer {
     case _ => env
   }
 
-  // name changed because of same type after type erasure
-  override def transformWithList(wl: List[WithItem])(implicit env: Env): (List[WithItem], Env) = 
-    wl.foldRight(List[WithItem](), env) {
-      case (w, (lw, e)) =>
-        val (wTrans, eTrans) = transform(w)(e)
-        (wTrans :: lw, eTrans)
-    }
-
   override def transform(w: WithItem)(implicit env: Env): (WithItem, Env) = w match {
     case WithItem(e, None) => (WithItem(transform(e), None), env)
     case WithItem(e, Some(asE)) => (env.get("tensor_flow"), e, asE) match {
@@ -449,139 +283,78 @@ object TransformerTape extends Transformer {
       case _ => (WithItem(transform(e), Some(asE)), env)
       }
   }
-  
-  override def transform(item: DictItem)(implicit env: Env): DictItem = item match {
-    case KVPair(k, v) => KVPair(k, transform(v))
-    case DoubleStarred(e) => DoubleStarred(e)
-  }
-
-  override def transform(mc: MatchCase)(implicit env: Env): MatchCase = mc match {
-    case MatchCase(pat, cond, body) =>
-      MatchCase(transform(pat), cond.map(transform), transform(body)._1)
-  }
-
-  override def transform(pat: Pattern)(implicit env: Env): Pattern = pat match {
-    case MatchValue(e) => MatchValue(transform(e))
-    case MatchSingleton(c) => MatchSingleton(c)
-    case MatchSeq(lpat) => MatchSeq(lpat.map(transform))
-    case MatchStar(opt) => MatchStar(opt)
-    case MatchMapping(map, opt) => MatchMapping(
-      map.map { case (e, pat) => (e, transform(pat)) },
-      opt
-    )
-    case MatchClass(e, lpat, map) => MatchClass(e,
-      lpat.map(transform),
-      map.map { case (k, v) => (k, transform(v)) }
-    )
-    case MatchAs(opt, x) => MatchAs(opt.map(transform), x)
-    case MatchOr(lpat) => MatchOr(lpat.map(transform))
-    case MatchWildcard => MatchWildcard
-    case MatchGroup(p) => MatchGroup(transform(p))
-  }
-
-  /////////////////////////////////////////
-  // helper functions
-  /////////////////////////////////////////
-  override def parseStmts(code: String): List[Stmt] = {
-    TokenListParser(tokenizeText(code)).body
-  }
-
-  override def parseExpr(str: String): Expr = {
-    val stmts = parseStmts(str)
-    stmts.headOption match {
-      case Some(ExprStmt(e)) if stmts.size == 1 => e
-      case _ => ???
-    }
-  }
-
-  // TODO: need new id gen algorithm
-  override def newId: Id = Id("id_new")
-
-  override def findKwarg(lk: List[Kwarg], str: String): Option[NormalKwarg] =
-    (lk.find {
-      case NormalKwarg(Id(x), _) if x == str => true
-      case _ => false
-    }).asInstanceOf[Option[NormalKwarg]]
-
-  override def replaceElement[T](lk: List[T], from: T, to: T): List[T] = {
-    lk.zipWithIndex.find(e => e._1 == from) match {
-      case Some((e, index)) =>
-        lk.slice(0, index) ++ (to :: lk.slice(index + 1, lk.length))
-      case None => lk
-    }
-  }
 
   /////////////////////////////////////////
   // Data needed for transformation
   // TODO this is actually static thingy...
   /////////////////////////////////////////
-//  val stmtData: Map[String, List[String] => String] = Map(
-//    // strict assign
-//    "assign-optimizer-some" -> (names => {
-//        val name = names(0)
-//        s"""global hvd_broadcast_done
-//           |if not hvd_broadcast_done:
-//           |  hvd.broadcast_variables(
-//           |    [x[1] for x in ${name}],
-//           |    root_rank=0
-//           |  )
-//           |  hvd_broadcast_done = True""".stripMargin
-//    }),
-//    // strict assign
-//    "assign-optimizer-none" -> (names => { 
-//        val name = names(0)
-//        s"""global hvd_broadcast_done
-//           |if not hvd_broadcast_done:
-//           |  hvd.broadcast_variables(
-//           |    [x[1] for x in ${name}],
-//           |    root_rank=0
-//           |  )
-//           |  hvd_broadcast_done = True""".stripMargin
-//    }),
-//    // import stmt
-//    "import-some" -> (names => { 
-//        val name = names(0)
-//        s"""import horovod.tensorflow as hvd
-//           |hvd_broadcast_done = False
-//           |hvd.init()
-//           |gpus = ${name}.config.experimental.list_physical_devices('GPU')
-//           |for gpu in gpus:
-//           |  ${name}.config.experimental.set_memory_growth(gpu, True)
-//           |if gpus:
-//           |  ${name}.config.experimental.\\
-//           |    set_visible_devices(gpus[hvd.local_rank()], 'GPU')""".stripMargin
-//    }),
-//    // expression stmt
-//    "expr-optimizer-some" -> (names => {
-//        val idz = names(0)
-//        val idt = names(1)
-//        s"""global hvd_broadcast_done
-//           |if not hvd_broadcast_done:
-//           |  hvd.broadcast_variables(
-//           |    [x[1] for x in ${idz}],
-//           |    root_rank=0
-//           |  )
-//           |  hvd.broadcast_variables(
-//           |    ${idt}.variables(),
-//           |    root_rank=0
-//           |  )
-//           |  hvd_broadcast_done = True""".stripMargin
-//    }),
-//    // expression stmt
-//    "expr-optimizer-none" -> (names => { 
-//        val idz = names(0)
-//        val idt = names(1)
-//        s"""global hvd_broadcast_done
-//           |if not hvd_broadcast_done:
-//           |  hvd.broadcast_variables(
-//           |    [x[1] for x in ${idz}],
-//           |    root_rank=0
-//           |  )
-//           |  hvd.broadcast_variables(
-//           |    ${idt}.variables(),
-//           |    root_rank=0
-//           |  )
-//           |  hvd_broadcast_done = True""".stripMargin
-//    }),
-//  )
+  val stmtData: Map[String, List[String] => String] = Map(
+    // strict assign
+    "assign-optimizer-some" -> (names => {
+        val name = names(0)
+        s"""global hvd_broadcast_done
+           |if not hvd_broadcast_done:
+           |  hvd.broadcast_variables(
+           |    [x[1] for x in ${name}],
+           |    root_rank=0
+           |  )
+           |  hvd_broadcast_done = True""".stripMargin
+    }),
+    // strict assign
+    "assign-optimizer-none" -> (names => { 
+        val name = names(0)
+        s"""global hvd_broadcast_done
+           |if not hvd_broadcast_done:
+           |  hvd.broadcast_variables(
+           |    [x[1] for x in ${name}],
+           |    root_rank=0
+           |  )
+           |  hvd_broadcast_done = True""".stripMargin
+    }),
+    // import stmt
+    "import-some" -> (names => { 
+        val name = names(0)
+        s"""import horovod.tensorflow as hvd
+           |hvd_broadcast_done = False
+           |hvd.init()
+           |gpus = ${name}.config.experimental.list_physical_devices('GPU')
+           |for gpu in gpus:
+           |  ${name}.config.experimental.set_memory_growth(gpu, True)
+           |if gpus:
+           |  ${name}.config.experimental.\\
+           |    set_visible_devices(gpus[hvd.local_rank()], 'GPU')""".stripMargin
+    }),
+    // expression stmt
+    "expr-optimizer-some" -> (names => {
+        val idz = names(0)
+        val idt = names(1)
+        s"""global hvd_broadcast_done
+           |if not hvd_broadcast_done:
+           |  hvd.broadcast_variables(
+           |    [x[1] for x in ${idz}],
+           |    root_rank=0
+           |  )
+           |  hvd.broadcast_variables(
+           |    ${idt}.variables(),
+           |    root_rank=0
+           |  )
+           |  hvd_broadcast_done = True""".stripMargin
+    }),
+    // expression stmt
+    "expr-optimizer-none" -> (names => { 
+        val idz = names(0)
+        val idt = names(1)
+        s"""global hvd_broadcast_done
+           |if not hvd_broadcast_done:
+           |  hvd.broadcast_variables(
+           |    [x[1] for x in ${idz}],
+           |    root_rank=0
+           |  )
+           |  hvd.broadcast_variables(
+           |    ${idt}.variables(),
+           |    root_rank=0
+           |  )
+           |  hvd_broadcast_done = True""".stripMargin
+    }),
+  )
 }
