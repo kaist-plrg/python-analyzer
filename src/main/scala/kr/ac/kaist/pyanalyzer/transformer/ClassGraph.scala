7e97fa3f12635cb574b8a5a89d48606c419e02a0
Yusung Sim <yusungsim@kaist.ac.kr>
2021-10-15 16:59:22 +0900

    Immutable ClassGraph class

--- src/main/scala/kr/ac/kaist/pyanalyzer/transformer/ClassGraph.scala
+++ src/main/scala/kr/ac/kaist/pyanalyzer/transformer/ClassGraph.scala
@@ -1,56 +1,62 @@
 package kr.ac.kaist.pyanalyzer.transformer
 
 import kr.ac.kaist.pyanalyzer.parser.ast._
-import kr.ac.kaist.pyanalyzer.util.Useful.{ accUntil }
 
 // node representing module or class
 // edge : child -> parent
 // no multiple inheritance => only 1 parent per child
-case class ClassNode(name: String) 
+case class ClassNode(name: String, parent: Option[ClassNode]) 
 
-class ClassGraph {
-  // representing graph of ClasNodes
-  var nodes = List[ClassNode]()
-  var edges = Map[Int,Int]() // (child -> parent)
+case class ClassGraph(nodes: List[ClassNode], vars: Map[String, ClassNode]) {
+  // addNode
+  def addNode(node: ClassNode): ClassGraph =
+    ClassGraph(nodes :+ node, vars)
 
-  def addNode(clsname: String): Unit = { nodes = nodes :+ ClassNode(clsname) } 
-  def addEdge(from: Int, to: Int): Unit = { edges = edges + (from -> to) }
+  def addNode(nodeName: String): ClassGraph =
+    ClassGraph(nodes :+ ClassNode(nodeName, None), vars)
 
-  def existsName(name: String): Boolean = nodes contains ClassNode(name)
-  def getId(node: ClassNode): Int = nodes.indexOf(node)
-  def getNode(id: Int) = nodes(id)
-  
-  def getParentId(id: Int): Option[Int] = edges get id
-  def getParentIds(id: Int): List[Int] = accUntil(id)(getParentId(_))
+  def addNode(nodeName: String, parent: ClassNode): ClassGraph =
+    ClassGraph(nodes :+ ClassNode(nodeName, Some(parent)), vars)
 
-  def addAccessPath(path: List[String]): Unit =
-    path.foreach(s => { if (!existsName(s)) addNode(s) })
-
-  // representing map from variables to ClassNode
-  var varMap = Map[String, Int]()
-
-  def addVar(name: String, id: Int): Unit = { varMap = varMap + (name -> id) }
-
-  def addModuleImport(alias: Alias): Unit = alias match { 
-    case Alias(name, asName) =>
-      addAccessPath(alias.name.map(id => id.name)) 
-      addVar(
-        asName.getOrElse(alias.name.last).name,  // asName or last Id of name
-        getId(ClassNode(name.last.name))) // index of node of name.last
-  }
-
-  def procImport(stmt: ImportStmt): Unit = stmt match {
-    case ImportStmt(as) => as.foreach(addModuleImport)
-  }
-
-  def attrFlatten(attr: Expr): (Expr, List[String]) = ??? 
-
-  def procAssign(stmt: AssignStmt): Unit = stmt match {
-    case AssignStmt(List(EName(x)), expr, _) => attrFlatten(expr) match {
-      case (EName(v), l) =>
-        val node = getNode(varMap(v.name))
-        // TODO check the l: List[access_path_tokens] is valid and get corresponding node
-        // TODO : addVar(x, corresponding node id)
+  def addNode(nodeName: String, pOpt: Option[ClassNode]): ClassGraph =
+    pOpt match {
+      case None => addNode(nodeName)
+      case Some(parent) => addNode(nodeName, parent)
     }
+ 
+  // addVar
+  def addVar(varName: String, node: ClassNode) =
+    ClassGraph(nodes, vars + (varName -> node))
+
+  // ischild: checks if targetNode has has a child with name = id
+  // returns the child node if found, else None
+  def isChild(targetNode: ClassNode, id: String): Option[ClassNode] =
+    nodes.filter(node => node.name == id && node.parent == Some(targetNode)).headOption
+
+  // getPath
+  def getPath(expr: Expr): (List[String], ClassGraph, ClassNode) = expr match {
+    // expr.id case
+    case Attribute(expr, id) => {
+      val (l, newGraph, pnode) = getPath(expr)
+      isChild(pnode, id.name) match {
+        case Some(cnode) => (l :+ id.name, newGraph, cnode) 
+        case None => {
+          val cnode = ClassNode(id.name, Some(pnode))
+          (l :+ id.name, newGraph.addNode(cnode), cnode)
+        }
+      }
+    }
+    // id case
+    case EName(id) => {
+      (vars get id.name) match {
+        case Some(node) => (List(id.name), this, node) 
+        case None => {
+          val node = ClassNode(id.name, None)
+          (List(id.name), this.addNode(node), node)
+        }
+      }
+    }
+    // others: should not appear
+    case _ => ???
   }
 } 
