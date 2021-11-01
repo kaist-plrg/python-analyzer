790fbadde4ee2cfe37c72251529ab642226fb20f
Yusung Sim <yusungsim@kaist.ac.kr>
2021-11-01 17:55:48 +0900

    Impl. TransformPipe for filename given case

--- src/main/scala/kr/ac/kaist/pyanalyzer/pipeline/CheckFilePipe.scala
+++ src/main/scala/kr/ac/kaist/pyanalyzer/pipeline/CheckFilePipe.scala
@@ -3,5 +3,6 @@ package kr.ac.kaist.pyanalyzer.pipeline
 import java.io.File
 
 case object CheckFilePipe extends Pipeline[File, Option[String]] {
-  def execute(s: File): Option[String] = Option.when(s.isFile())(s.getName) 
+  def execute(s: File): Option[String] = 
+    Option.when(s.isFile())(s.getName().replaceFirst("[.][^.]+$", "")) 
 }
--- src/main/scala/kr/ac/kaist/pyanalyzer/pipeline/TransformPipe.scala
+++ src/main/scala/kr/ac/kaist/pyanalyzer/pipeline/TransformPipe.scala
@@ -5,19 +5,54 @@ import kr.ac.kaist.pyanalyzer.transformer.ClassOrder
 import kr.ac.kaist.pyanalyzer.transformer._
 import kr.ac.kaist.pyanalyzer.transformer.Transformer
 import kr.ac.kaist.pyanalyzer.transformer.Env
-import kr.ac.kaist.pyanalyzer.util.{ Info }
+import kr.ac.kaist.pyanalyzer.util.{ Info, FileInfo, DirInfo }
 
-case object TransformPipe 
-extends Pipeline
-[((Info[(Module, ModuleSummary)], ClassOrder), Option[String]), Info[Module]] {
+case class NotDir(name: String) extends Exception(name)
+case class NotFile(name: String) extends Exception(name)
+
+case object TransformPipe extends Pipeline[
+  ((Info[(Module, ModuleSummary)], ClassOrder), Option[String]), 
+  Info[Module]
+] {
+  // some helpers
+  def removeDirSummary(i: DirInfo[(Module, ModuleSummary)]): DirInfo[Module] =
+    i.map(_._1) match {
+      case e: FileInfo[Module] => throw NotDir(i.name) 
+      case e: DirInfo[Module] => e
+    }
+  def removeFileSummary(i: FileInfo[(Module, ModuleSummary)]): FileInfo[Module] =
+    i.map(_._1) match {
+      case e: DirInfo[Module] => throw NotFile(i.name) 
+      case e: FileInfo[Module] => e
+    }
+
+
   def execute(
     p: ((Info[(Module, ModuleSummary)], ClassOrder), Option[String])
   ): Info[Module] = {
     val ((info, order), opt) = p   
-    // TODO impl
-    opt match {
-      case Some(fname) => ??? // target file path given 
-      case None => ??? // should find target file by ModuleSummary TLType
+    info match {
+      // should be DirInfo
+      case FileInfo(_, _) => throw NotDir(info.name)
+      case DirInfo(name, ds, fs) => opt match {
+        // target filename given
+        // assume: target file exist in fs (direct child)
+        case Some(fname) => 
+          DirInfo(
+            name, 
+            ds.map(di => removeDirSummary(di)), 
+            fs.map(fi => {
+            if (fi.name == fname) {
+              val (mod, summ) = fi.info
+              val transformedModule = Transformer(mod, order, summ.tl)
+              FileInfo[Module](fi.name, transformedModule)
+            } else {
+              removeFileSummary(fi)
+            }
+          }))
+        // should find target file by TLType
+        case None => ???
+      }   
     }
-  }
+  } 
 }
