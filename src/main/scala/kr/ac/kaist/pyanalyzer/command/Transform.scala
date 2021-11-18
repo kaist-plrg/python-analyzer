510e9c56737c1176100425afa03ef756c1d1f204
Wonho Shin <wonhoshin@iMac-4.local>
2021-11-18 16:58:23 +0900

    Add dump option for Transform command

--- src/main/scala/kr/ac/kaist/pyanalyzer/command/Transform.scala
+++ src/main/scala/kr/ac/kaist/pyanalyzer/command/Transform.scala
@@ -6,12 +6,15 @@ import kr.ac.kaist.pyanalyzer.parser.Tokenizer._
 import kr.ac.kaist.pyanalyzer.parser.TokenListParser
 import kr.ac.kaist.pyanalyzer.parser.ast.Beautifier._
 import kr.ac.kaist.pyanalyzer.parser.ast.Module
+import kr.ac.kaist.pyanalyzer.pipeline.TransformRunner._
 import kr.ac.kaist.pyanalyzer.transformer._
 import kr.ac.kaist.pyanalyzer.hierarchy.ClassOrder
+import kr.ac.kaist.pyanalyzer.hierarchy.ClassAnalysis._
 import kr.ac.kaist.pyanalyzer.transformer.Transformer
 import kr.ac.kaist.pyanalyzer.transformer.TrainingLoop
 import kr.ac.kaist.pyanalyzer.util.Useful._
 import kr.ac.kaist.pyanalyzer.util.Errors._
+import kr.ac.kaist.pyanalyzer.util.DirInfo
 import scala.Console._
 import scala.util.Try
 import java.io.File
@@ -25,9 +28,12 @@ object Transform {
       case e: Throwable => ".*".r
     }
     // diff files
-    val diff = optionMap.getOrElse("diff", "hvd-trans")
+    val diff = optionMap.getOrElse("diff", "hvd-hvd-trans")
+    lazy val diffParser = "[^-]*".r ~ ("-" ~> ".*".r)
+    val diffTarget = parse(diffParser, diff)
     // set diff option
     val diffOption = if (optionMap contains "y") "y" else "u"
+    val dump = if (optionMap contains "d") true else false
 
     val hvd = new File(HOROVOD_DIR)
 
@@ -36,81 +42,39 @@ object Transform {
       moduleDir <- versionDir.listFiles if moduleDir.isDirectory
       modelDir <- moduleDir.listFiles if modelDir.isDirectory
       modelPath = modelDir.toString
-    } {
-      lazy val printModelName = {
-        println
-        println(s"$MAGENTA${modelPath diff HOROVOD_DIR}$RESET")
-        println
+      if target matches modelPath
+    } try {
+      println
+      println(s"$MAGENTA${modelPath diff HOROVOD_DIR}$RESET")
+      println
+
+      val transInfo =
+        runPipe(s"$modelPath/org").asInstanceOf[DirInfo[Module]]
+        .copy(dirname=s"${modelPath diff HOROVOD_DIR}/hvd-trans")
+
+      // dump
+      if (dump) {
+        executeCmd(s"rm -rf /$modelPath/hvd-trans", modelPath)
+        dumpInfoModule(transInfo, new File(HOROVOD_DIR))
       }
 
-      // get modules in the model
-      val files = walkTree(modelPath)
-      val moduleOptions = for {
-        file <- files
-        fullPath = file.toString
-        path = fullPath diff modelPath
-        if path endsWith ".py"
-        if path startsWith "/org/"
-        if target matches fullPath
-      } try {
-        printModelName
-        val name = path.drop(5)
-        println
-        println(s"$CYAN<$name>$RESET")
-
-        val orgAst = parseFile(file.toString, fullPath diff HOROVOD_DIR)
-        val orgResult = beautify(orgAst)
-
-        // no CHA
-        val order = GIVEN_CLASS_ORDER
-
-        val tl = TrainingLoop(orgAst, order).tl
-
-        // transformed
-        val transformedAst = Transformer(orgAst, order, tl)
-        val transformedResult = beautify(transformedAst)
-        // hvd
-        val hvdAst = parseFile(s"$modelPath/hvd/$name")
-        val hvdResult = beautify(hvdAst)
-
-        // target diff
-        val comparePair = diff match {
-          case "org-hvd" =>
-            ("org", orgResult, "hvd", hvdResult)
-          case "org-trans" =>
-            ("org", orgResult, "trans", transformedResult)
-          case _ =>
-            ("hvd", hvdResult, "trans", transformedResult)
-        }
-
-        // print result
-        printDiff(comparePair, diffOption)
-      } catch {
-        case EmptyFileException =>
-        case e: Throwable => e.printStackTrace()
+      // diff
+      val (path1, path2) = diffTarget match {
+        case Success(a ~ b, _) => (s"$modelPath/$a", s"$modelPath/$b")
+        case _ => ???
       }
+      println(s"colordiff -$diffOption $path1 $path2")
+      try executeCmd(s"colordiff -$diffOption $path1 $path2")
+      catch {
+        case e: Throwable =>
+          executeCmd(
+            s"diff -$diffOption $path1 $path2"
+          )
+          println
+          println(s"${YELLOW}[Warning] install colordiff${RESET}")
+      }
+    } catch {
+      case e: Throwable => e.printStackTrace()
     }
-  }
-
-  def printDiff(
-    comparePair: (String, String, String, String),
-    diffOption: String
-  ): Unit = {
-    val (name1, content1, name2, content2) = comparePair
-    val path1 = s"$TRANS_LOG_DIR/$name1"
-    val path2 = s"$TRANS_LOG_DIR/$name2"
-    dumpFile(content1, path1)
-    dumpFile(content2, path2)
-    try executeCmd(
-      s"colordiff -$diffOption $path1 $path2"
-    ) catch {
-      case e: Throwable =>
-        executeCmd(
-          s"diff -$diffOption $path1 $path2"
-        )
-        println
-        println(s"${YELLOW}[Warning] install colordiff${RESET}")
-    }
-    executeCmd(s"rm $path1 $path2")
   }
 }
--- src/main/scala/kr/ac/kaist/pyanalyzer/pipeline/ParsePipe.scala
+++ src/main/scala/kr/ac/kaist/pyanalyzer/pipeline/ParsePipe.scala
@@ -1,17 +1,28 @@
 package kr.ac.kaist.pyanalyzer.pipeline
 
+import kr.ac.kaist.pyanalyzer._
 import kr.ac.kaist.pyanalyzer.pipeline.Pipeline
 import kr.ac.kaist.pyanalyzer.parser.SourceParser._
 import kr.ac.kaist.pyanalyzer.parser.ast._
-import kr.ac.kaist.pyanalyzer.util.Info
+import kr.ac.kaist.pyanalyzer.util.{ Info, DirInfo }
 import kr.ac.kaist.pyanalyzer.util.DirWalker._
 import java.io.File
 
 case object ParsePipe extends Pipeline[File, Info[Module]] {
-  def execute(path: File): Info[Module] = 
+  def execute(path: File): Info[Module] = {
+    val relPath = path.getPath() diff HOROVOD_DIR
     if (path.isFile()) {
-      walkFile(path.getParentFile())(parseFile(_))
+      walkFile(path.getParentFile())(parseFile(_)) match {
+        case dInfo: DirInfo[Module] =>
+          val name = relPath.slice(0, relPath indexOf "/org/")
+          dInfo.copy(dirname=name)
+        case _ => ???
+      }
     } else {
-      walkFile(path)(parseFile(_))
+      walkFile(path)(parseFile(_)) match {
+        case dInfo: DirInfo[Module] => dInfo.copy(dirname=relPath)
+        case _ => ???
+      }
     }
+  }
 }
